42cbfdf6fbe5cb0322a2a6963f367ff905f7780c
Zaid_Siddiqui <zaidahmadsiddiqui97@gmail.com>
2024-11-29 17:52:51 +0200

    contact service

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure this is correct
-  }
-})
+    outDir: 'dist', // Ensure this is correct
+    rollupOptions: {
+      external: ['@emailjs/browser'], // Externalize @emailjs/browser
+    },
+  },
+});
