1a0451d78ff98a0832d857f888bec47433d8c780
Zaid_Siddiqui <zaidahmadsiddiqui97@gmail.com>
2024-11-29 17:01:45 +0200

    contact service

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -16,7 +16,7 @@ const ContactUs = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/send-email", {
+            const response = await fetch("/send-email",{
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
